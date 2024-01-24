a8c53c446e438e58d1dd5e34c7d1ec69877a00f1
Nirosha55 <niroshaniru323@gmail.com>
2024-01-24 18:41:47 +0530

    frstcommit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+    <div>
+    <h1>My Contact Lists</h1>
+    <h2>Nirosha</h2>
+    <img src='https://w7.pngwing.com/pngs/46/759/png-transparent-chili-dog-hot-dog-days-corn-dog-cheese-dog-hot-dog-food-beef-cheese-thumbnail.png' width="150" height="80"></img>
+   <p>+7799530272</p>
+   <p>nirosha@gmail.com</p>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   <h2>Vanshika</h2>
+    <img src='https://png.pngtree.com/png-clipart/20210310/original/pngtree-cute-samoyed-pet-dog-png-image_5965827.jpg' width="150" height="80"></img>
+   <p>+8019530272</p>
+   <p>vanshu@gmail.com</p>
+
+   <h2>Varnika</h2>
+    <img src='https://png.pngtree.com/png-clipart/20210310/original/pngtree-cute-samoyed-pet-dog-png-image_5965827.jpg' width="150" height="80"></img>
+   <p>+8019530272</p>
+   <p>bondu@gmail.com</p>
+    </div>
+
+  ,document.getElementById('root'));
+
