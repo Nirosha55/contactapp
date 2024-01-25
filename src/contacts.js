9e59a525ce3b1b6b1fcd680697f45f032d152e8d
Nirosha55 <niroshaniru323@gmail.com>
2024-01-25 14:31:53 +0530

    scndcommit

---
+++ src/contacts.js
@@ -0,0 +1,39 @@
+const contacts = [
+  {
+    name: "Nirosha",
+    imgURL:
+      "https://spng.pngfind.com/pngs/s/155-1554173_free-png-pink-rose-png-images-transparent-transparent.png",
+    phone: "+779 953 0272",
+    email: "nirosha@gmail.com"
+  },
+  {
+    name: "Vanshika",
+    imgURL:
+      "https://pngimg.com/d/rose_PNG637.png",
+    phone: "+9000 420 062",
+    email: "vanshu@gmail.com"
+  },
+  {
+    name: "Varnika",
+    imgURL:
+      "https://i.pinimg.com/originals/db/cc/35/dbcc350949425c88fe53a9aa3ce8bad5.png",
+    phone: "+8019 530 272",
+    email: "bondu@gmail.com"
+  },
+  {
+    name: "Vihanth",
+    imgURL:
+      "https://png.pngtree.com/png-clipart/20201128/ourmid/pngtree-pink-rose-flower-png-image_2451638.jpg",
+    phone: "+823 999 8327",
+    email: "chinnu@gmail.com"
+  },
+  {
+    name: "Ishika",
+    imgURL:
+      "https://img.freepik.com/free-psd/fresh-pink-rose-isolated-transparent-background_191095-13926.jpg",
+    phone: "+630 294 5424",
+    email: "chalsi@gmail.com"
+  }
+];
+
+export default contacts;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/App';
 
-ReactDOM.render(
-    <div>
-    <h1>My Contact Lists</h1>
-    <h2>Nirosha</h2>
-    <img src='https://w7.pngwing.com/pngs/46/759/png-transparent-chili-dog-hot-dog-days-corn-dog-cheese-dog-hot-dog-food-beef-cheese-thumbnail.png' width="150" height="80"></img>
-   <p>+7799530272</p>
-   <p>nirosha@gmail.com</p>
-
-   <h2>Vanshika</h2>
-    <img src='https://png.pngtree.com/png-clipart/20210310/original/pngtree-cute-samoyed-pet-dog-png-image_5965827.jpg' width="150" height="80"></img>
-   <p>+8019530272</p>
-   <p>vanshu@gmail.com</p>
-
-   <h2>Varnika</h2>
-    <img src='https://png.pngtree.com/png-clipart/20210310/original/pngtree-cute-samoyed-pet-dog-png-image_5965827.jpg' width="150" height="80"></img>
-   <p>+8019530272</p>
-   <p>bondu@gmail.com</p>
-    </div>
-
-  ,document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
